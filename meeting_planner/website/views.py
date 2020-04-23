a8bf86e7c1c14ea9782c61722744ce7b20647255
cimpoes.andrei.alexandru@gmail.com <cimpoes.andrei.alexandru@gmail.com>
2020-04-23 19:59:54 +0300

    Add another view to see the datetime

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -1,5 +1,13 @@
+from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+
+
+
+
 def welcome(request):
     return HttpResponse("Welcome to the Meeting Planner!")
+
+
+def date(request):
+    return HttpResponse("This page was running at the:" + str(datetime.now()))
