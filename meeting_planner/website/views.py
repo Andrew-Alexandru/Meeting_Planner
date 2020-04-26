8e2e5ce240b2b276c536b89a6c2739d561e3288c
cimpoes.andrei.alexandru@gmail.com <cimpoes.andrei.alexandru@gmail.com>
2020-04-26 17:21:33 +0300

    Add a about page

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -3,11 +3,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-
-
 def welcome(request):
     return HttpResponse("Welcome to the Meeting Planner!")
 
 
 def date(request):
     return HttpResponse("This page was running at the:" + str(datetime.now()))
+
+
+def about(request):
+    return HttpResponse("I am Andrei and I want to learn Django!")
