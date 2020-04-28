86356a5f66020a738521191d3165577db782ab72
cimpoes.andrei.alexandru@gmail.com <cimpoes.andrei.alexandru@gmail.com>
2020-04-28 18:58:47 +0300

    Adding path to meetings

--- meeting_planner/meetings/views.py
+++ meeting_planner/meetings/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Meeting
 
-# Create your views here.
+
+def detail(request, id):
+    # id is the id of the meeting to show on page
+    meeting = Meeting.objects.get(pk=id)
+    # pk is primary key ->row with this unique id
+    return render(request, 'meetings/detail.html', {'meeting': meeting})
