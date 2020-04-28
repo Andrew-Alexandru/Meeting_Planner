b19cc624bc9bd03d036d88ee28279bdb9556f2f0
cimpoes.andrei.alexandru@gmail.com <cimpoes.andrei.alexandru@gmail.com>
2020-04-28 19:05:13 +0300

    Add a 404 error if doesn't exist

--- meeting_planner/meetings/views.py
+++ meeting_planner/meetings/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Meeting
 
 
 def detail(request, id):
     # id is the id of the meeting to show on page
-    meeting = Meeting.objects.get(pk=id)
+    meeting = get_object_or_404(Meeting, pk=id)
     # pk is primary key ->row with this unique id
     return render(request, 'meetings/detail.html', {'meeting': meeting})
